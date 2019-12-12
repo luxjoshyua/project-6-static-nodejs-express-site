b625d833193229920153ef94a6edb99dd3a4fdf1
Joshua Fielding <Joshua.Fielding@fairfaxmedia.com.au>
2019-12-12 15:45:04 +1100

    added app.js file

--- app.js
+++ app.js
@@ -1,9 +1,46 @@
-// import express
+// import express + require express
 const express = require('express');
-
+// import body-parser and require
+const bodyParser = require('body-parser'); 
+// require the data json file
+const { projects } = require('data.json');
+// require the path module which can be used when setting the absolute path in the express.static function
+const path = require('path');
 // initialise express
 const app = express();
 
-
-// reference the public folder and its children, so the stylesheets work
+// Setup middleware
+// set `view engine` to `pug`
+app.set('view engine', 'pug');
+// use a static route and the express.static method to serve the static files located in the public folder
 app.use('/static', express.static('public'));
+
+// Setup routes
+
+// An "index" route (/) to render the "Home" page with the locals set to data.projects
+router.get('/', (req, res) => {
+    res.render('index')
+    app.locals = projects; 
+})
+
+// An "about" route (/about) to render the "About" page
+router.get('/about', (req, res) => {
+    res.render('/about');
+})
+
+// Dynamic "project" routes (/project or /projects) based on the id of the project that render a customized version 
+// of the Pug project template to show off each project. 
+
+// Which means adding data, or "locals", as an object that contains 
+// data to be passed to the Pug template
+
+router.get('/project/:id', (req, res) => {
+    const { data } = 
+    console.log('ID:', req.params.id)
+    // get the id from the url using reqs.params
+    id = req.params.id
+
+    
+
+    // loop through data
+})  
