b4b9be0111f71abb9be66c2d05e3a4d81b65655a
Joshua Fielding <Joshua.Fielding@fairfaxmedia.com.au>
2019-12-18 15:30:06 +1100

    added error template for projects

--- routes/error.js
+++ routes/error.js
@@ -6,14 +6,9 @@ const projects = require('../data.json');
  * 404 and Global Error Handlers
  */
 router.all('*', (req, res, next) => {
-    const projectsLoop = Object.values(projects);
-    projectsLoop.forEach(project => {
-        if (req.url === `/project/${project.id}`) {
-            return next();
-        }
-    });
+  
     // Create new error to handle non-existent routes
-    const err = new Error('err');
+    const err = new Error('Page not found!');
     err.status = 404;
     console.log(`Something went wrong. Status: ${err.status}, Message: ${err.message}, Stack: ${err.stack}`)
     // Pass error to global error handler below
@@ -21,9 +16,10 @@ router.all('*', (req, res, next) => {
 });
 
 router.use((err, req, res, next) => {
-    res.locals.error = err;
-    res.render('error')
-})
+    res.render('error', {
+        error : err
+    });
+});
 
 // Export error handlers
 module.exports = router;
--- routes/projects.js
+++ routes/projects.js
@@ -5,7 +5,7 @@ const data = require('../data.json');
 
 // Dynamic "project" routes (/project or /projects) based on the id of the project that render a customized version 
 // of the Pug project template to show off each project. 
-router.get('/projects/:paramId', (req, res) => {
+router.get('/projects/:paramId', (req, res, next) => {
     // add data or 'locals' as an object that contains data to be passed to the Pug template
     const { paramId } = req.params;
     // loop through projects
@@ -14,14 +14,18 @@ router.get('/projects/:paramId', (req, res) => {
         // console.log(i);
         // if the project id matches the url parameter id
         if (data.projects[i].id === paramId) {
-            // console.log("The id matches " + paramId)
             // return the template that matches that id
             return res.render('project', data.projects[i]);
         }
         // console.log("The id doesn't match")
     }
+
     // res.send() sends a message to the client e.g. browser
-    return res.send('error');
+    // return res.send("<h1>Apologies, that project doesn't exist, yet!</h1>");
+
+    return next();
+    
+    
 })
 
 // export the router so we can use it in the main app.js file
